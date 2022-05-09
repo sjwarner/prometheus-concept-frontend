5e50f0a861ff267b4db5303b49ad059c6f65a29f
Sam Warner <sam_warner@talktalk.net>
2022-05-09 23:47:29 +0100

    Rename local multiplayer directory, consistency in pages

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 
 import Homepage from "./pages/Homepage/Homepage";
-import LocalMultiplayerPage from "./pages/LocalMultiplayerPage/LocalMultiplayerPage";
+import LocalMultiplayerPage from "./pages/LocalMultiplayer/LocalMultiplayerPage";
 import CreateGamePage from "./pages/CreateGame/CreateGamePage";
 
 function App() {
