a0753a300e349099c8681610d48b57d0869e6cbe
Sam Warner <sam_warner@talktalk.net>
2022-05-15 10:40:58 +0100

    Make leader join party when generating room

--- src/pages/CreateGame/CreateGamePage.jsx
+++ src/pages/CreateGame/CreateGamePage.jsx
@@ -1,15 +1,29 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import axios from "axios";
+import { io } from "socket.io-client";
 
 const CreateGamePage = () => {
     // const baseUrl = process.env.REACT_APP_BACKEND_URL || "http://localhost:8000";
     const baseUrl = "http://localhost:8000";
+    const [socket, setSocket] = useState(null);
     const [username, setUsername] = useState("");
-    const [roomCode, setRoomCode] = useState(undefined);
+    const [roomCode, setRoomCode] = useState(null);
+    const [isInRoom, setIsInRoom] = useState(false);
+    const [canStart, setCanStart] = useState(false);
+    const [players, setPlayers] = useState(null);
 
     const [errorMessage, setErrorMessage] = useState("");
     const [hasError, setHasError] = useState(false);
     const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+        const newSocket = io.connect(`${baseUrl}/${roomCode}`, {
+            transports: ["websocket"]
+        });
+
+        console.log(newSocket);
+        setSocket(newSocket);
+    }, [baseUrl, roomCode])
 
     const createParty = (username) => {
         if (!username) {
@@ -26,7 +40,7 @@ const CreateGamePage = () => {
             .then((res) => {
                 setIsLoading(false);
                 setRoomCode(res.data.namespace);
-                // joinParty();
+                joinParty();
             })
             .catch(err => {
                 console.log(err)
@@ -35,6 +49,44 @@ const CreateGamePage = () => {
                 setHasError(true );
             });
     }
+
+    const joinParty = () => {
+        console.log("socket created");
+        socket.emit("setName", username);
+
+        socket.on("joinSuccess", () => {
+            console.log("join successful");
+            setIsLoading(false);
+            setIsInRoom(true);
+        });
+
+        socket.on("joinFailed", (err) => {
+            console.log("join failed, cause: " + err);
+            setIsLoading(false);
+        });
+
+        socket.on("leader", function () {
+            console.log("You are the leader");
+        });
+
+        socket.on("partyUpdate", (players) => {
+            console.log(players);
+            setPlayers(players);
+
+            const readyPlayers = players?.map(player => player.isReady).filter((isReady) => isReady === true).length;
+
+            if (players.length === 2 && readyPlayers === players.length) {
+                this.setState({ canStart: true });
+                setCanStart(true);
+            } else {
+                setCanStart(false);
+            }
+        });
+
+        socket.on("disconnected", function () {
+            console.log("You've lost connection with the server");
+        });
+    };
 
     return(
         <div className="app p-8 flex flex-col justify-center items-center h-screen">
