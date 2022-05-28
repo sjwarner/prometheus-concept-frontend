c82eaa6716b14de0e3458ff8785c58edcfba7770
Sam Warner <sam_warner@talktalk.net>
2022-05-28 13:36:25 +0100

    Hide page create game fields when game has started. Disable fields after submission.

--- src/pages/CreateGame/CreateGamePage.jsx
+++ src/pages/CreateGame/CreateGamePage.jsx
@@ -107,43 +107,52 @@ const CreateGamePage = () => {
     return(
         <div className="app p-8 flex flex-col justify-center items-center h-screen">
             <div className="mb-4">
-                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
-                    Username
-                </label>
-                <input
-                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    id="username" type="text" placeholder="Username" onChange={(evt) => setUsername(evt.target.value)} />
-                {hasError && (
-                    <div className="mt-4">{errorMessage}</div>
-                )}
-                {!isLoading && !roomCode && (
-                    <button className="block m-auto mt-4 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
-                            onClick={() => createParty(username)}>
-                        Create game
-                    </button>
+                {!isGameStarted && (
+                    <>
+                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
+                            Username
+                        </label>
+                        <input
+                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            id="username" type="text" placeholder="Username" disabled={isInRoom}
+                            onChange={(evt) => setUsername(evt.target.value)} />
+                        {hasError && (
+                            <div className="mt-4">{errorMessage}</div>
+                        )}
+                        {!isLoading && !roomCode && (
+                            <button className="block m-auto mt-4 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+                                    onClick={() => createParty(username)}>
+                                Create game
+                            </button>
+                        )}
+
+                        {roomCode && (
+                            <div>
+                                <p>
+                                    ROOM CODE: <br></br> <br></br>
+                                    <b className="" onClick={() => {}}>
+                                        {roomCode}
+                                    </b>
+                                </p>
+                                {/*{this.state.copied ? <p>Copied to clipboard</p> : null}*/}
+                            </div>
+                        )}
+
+                        {isInRoom && (
+                            <PlayerList players={players} setPlayers={setPlayers} />
+                        )}
+
+                        {canStart && (
+                            <button className="block m-auto mt-4 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+                                    onClick={() => startGame()}>
+                                Start Game
+                            </button>
+                        )}
+                    </>
                 )}
 
-                {roomCode && (
-                    <div>
-                        <p>
-                            ROOM CODE: <br></br> <br></br>
-                            <b className="" onClick={() => {}}>
-                                {roomCode}
-                            </b>
-                        </p>
-                        {/*{this.state.copied ? <p>Copied to clipboard</p> : null}*/}
-                    </div>
-                )}
-
-                {isInRoom && (
-                    <PlayerList players={players} setPlayers={setPlayers} />
-                )}
-
-                {canStart && (
-                    <button className="block m-auto mt-4 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
-                            onClick={() => startGame()}>
-                        Start Game
-                    </button>
+                {isGameStarted && (
+                    <p>Game started!</p>
                 )}
             </div>
         </div>
