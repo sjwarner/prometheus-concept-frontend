401b07d603b06946c9cbad269015fd5e782b81dd
Sam Warner <sam_warner@talktalk.net>
2022-05-28 13:37:19 +0100

    Hide join game page fields when game has started. Disable fields after submission.

--- src/pages/JoinGame/JoinGamePage.jsx
+++ src/pages/JoinGame/JoinGamePage.jsx
@@ -125,42 +125,55 @@ const JoinGamePage = () => {
         });
     };
 
-    return(
+    return (
         <div className="app p-8 flex flex-col justify-center items-center h-screen">
             <div className="mb-4">
-                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
-                    Username
-                </label>
-                <input
-                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    id="username" type="text" placeholder="Username" onChange={(evt) => setUsername(evt.target.value)} />
-                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
-                    Room Code
-                </label>
-                <input
-                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    id="roomCode" type="text" placeholder="Room Code" onChange={(evt) => setRoomCode(evt.target.value)} />
-                {hasError && (
-                    <div className="mt-4">{errorMessage}</div>
-                )}
-                {!isLoading && !isInRoom && (
-                    <button
-                        className="block m-auto mt-4 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
-                        onClick={() => attemptJoinParty()}
-                        disabled={isLoading}
-                    >
-                        Join game
-                    </button>
+                {!isGameStarted && (
+                    <>
+                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
+                            Username
+                        </label>
+                        <input
+                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            id="username" type="text" placeholder="Username" disabled={isInRoom}
+                            onChange={(evt) => setUsername(evt.target.value)}/>
+                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
+                            Room Code
+                        </label>
+                        <input
+                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            id="roomCode" type="text" placeholder="Room Code" disabled={isInRoom}
+                            onChange={(evt) => setRoomCode(evt.target.value)}/>
+                        {hasError && (
+                            <div className="mt-4">{errorMessage}</div>
+                        )}
+                        {!isLoading && !isInRoom && (
+                            <button
+                                className="block m-auto mt-4 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+                                onClick={() => attemptJoinParty()}
+                                disabled={isLoading}
+                            >
+                                Join game
+                            </button>
+                        )}
+
+                        {isInRoom && !isReady && (
+                            <button
+                                className="block m-auto mt-4 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+                                onClick={() => reportReady()}
+                                disabled={isReady}
+                            >
+                                Ready up!
+                            </button>
+                        )}
+                        {isInRoom && isReady && (
+                            <p className="block text-gray-700 m-4">Waiting for host...</p>
+                        )}
+                    </>
                 )}
 
-                {isInRoom && (
-                    <button
-                        className="block m-auto mt-4 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
-                        onClick={() => reportReady()}
-                        disabled={isReady}
-                    >
-                        Ready up!
-                    </button>
+                {isGameStarted && (
+                    <p>Game started!</p>
                 )}
             </div>
         </div>
