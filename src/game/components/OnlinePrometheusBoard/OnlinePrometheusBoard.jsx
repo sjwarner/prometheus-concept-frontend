7116fe8141941f7feed8b672a2c18a2311322dfa
Sam Warner <sam_warner@talktalk.net>
2022-05-28 20:20:29 +0100

    Emit moved piece event after moving piece

--- src/game/components/OnlinePrometheusBoard/OnlinePrometheusBoard.jsx
+++ src/game/components/OnlinePrometheusBoard/OnlinePrometheusBoard.jsx
@@ -136,6 +136,8 @@ const OnlinePrometheusBoard = ({
       setOriginRank(null);
       setOriginFile(null);
       setValidMoves([]);
+
+      socket.emit("playerMovedPiece", gameState);
     }
   };
 
