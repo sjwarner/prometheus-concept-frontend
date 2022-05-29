85821bbb4313137b753de5a4668d5aee50d4b9e3
Sam Warner <sam_warner@talktalk.net>
2022-05-29 21:11:06 +0100

    Extract header component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import "./App.css";
 
 import Homepage from "./pages/Homepage/Homepage";
@@ -8,20 +8,12 @@ import CreateGamePage from "./pages/CreateGame/CreateGamePage";
 import JoinGamePage from "./pages/JoinGame/JoinGamePage";
 import NotFoundPage from "./pages/NotFound/NotFoundPage";
 import Footer from "./general/components/Footer/Footer";
-import Tetrahedron from "./game/components/Pieces/Tetrahedron";
+import Header from "./general/components/Header/Header";
 
 function App() {
   return (
       <div className="flex flex-col h-screen justify-between">
-          <header className="py-5 bg-black text-white text-center flex flex-row justify-between">
-              <div className="h-6 w-6 ml-8 mr-8">
-                  <Tetrahedron />
-              </div>
-              Prometheus Concept
-              <div className="h-6 w-6 ml-8 mr-8">
-                  <Tetrahedron />
-              </div>
-          </header>
+          <Header />
           <main>
               <Router>
                   <div>
