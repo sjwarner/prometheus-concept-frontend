dbaae0c915b96cfa4de558c8841a4b01f0313fa6
Sam Warner <sam_warner@talktalk.net>
2022-05-29 14:25:58 +0100

    Extract LocalBoardCaption to component

---
+++ src/game/components/LocalBoardCaption/LocalBoardCaption.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import Players from "../../logic/Players";
+import { InitialGameStateWhite } from "../../logic/InitialGameState";
+
+const LocalBoardCaption = ({
+  inProgress,
+  setInProgress,
+  winner,
+  setWinner,
+  setGameState,
+  turn,
+  playerOneSpherePlaced,
+  setPlayerOneSpherePlaced,
+  playerTwoSpherePlaced,
+  setPlayerTwoSpherePlaced,
+}) => {
+  const resetGame = () => {
+    setInProgress(true);
+    setWinner(null);
+    setGameState(JSON.parse(JSON.stringify(InitialGameStateWhite)));
+    setPlayerOneSpherePlaced(false);
+    setPlayerTwoSpherePlaced(false);
+  };
+
+  const inProgressWithSpheresPlaced =
+    inProgress && playerOneSpherePlaced && playerTwoSpherePlaced;
+
+  console.log(inProgress);
+
+  return (
+    <div className="below-board-container mt-4">
+      {winner && (
+        <span className="block m-auto mt-4 bg-white text-gray-800 py-2 px-4">
+          🎉 {winner === Players.PLAYER_ONE ? "Player One" : "Player Two"} won!
+          🎉
+        </span>
+      )}
+      {inProgress && !playerOneSpherePlaced && (
+        <span className="block m-auto mt-4 bg-white text-gray-800 py-2 px-4">
+          Place Player One's sphere.
+        </span>
+      )}
+      {inProgress && playerOneSpherePlaced && !playerTwoSpherePlaced && (
+        <span className="block m-auto mt-4 bg-white text-gray-800 py-2 px-4">
+          Place Player Two's sphere.
+        </span>
+      )}
+      {inProgressWithSpheresPlaced && (
+        <span className="block m-auto mt-4 bg-white text-gray-800 py-2 px-4">
+          {turn === Players.PLAYER_ONE ? "White" : "Black"}'s turn.
+        </span>
+      )}
+      {!inProgress && (
+        <button
+          className="block m-auto mt-4 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+          onClick={() => resetGame()}
+        >
+          {winner ? "Play again?" : "Start!"}
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default LocalBoardCaption;
--- src/game/components/PrometheusBoard/PrometheusBoard.jsx
+++ src/game/components/PrometheusBoard/PrometheusBoard.jsx
@@ -3,7 +3,7 @@ import PrometheusSquare from "../PrometheusSquare/PrometheusSquare";
 import Pieces from "../../logic/Pieces";
 import Players from "../../logic/Players";
 import { calculateValidMoves, isArrayInArray } from "../../logic/utils";
-import { InitialGameStateWhite } from "../../logic/InitialGameState";
+import LocalBoardCaption from "../LocalBoardCaption/LocalBoardCaption";
 
 const PrometheusBoard = ({
   inProgress,
@@ -117,17 +117,6 @@ const PrometheusBoard = ({
     }
   };
 
-  const inProgressWithSpheresPlaced =
-    inProgress && playerOneSpherePlaced && playerTwoSpherePlaced;
-
-  const resetGame = () => {
-    setInProgress(true);
-    setWinner(null);
-    setGameState(JSON.parse(JSON.stringify(InitialGameStateWhite)));
-    setPlayerOneSpherePlaced(false);
-    setPlayerTwoSpherePlaced(false);
-  };
-
   return (
     <div className="board">
       <div className="content">
@@ -155,37 +144,18 @@ const PrometheusBoard = ({
               </div>
             );
           })}
-        <div className="below-board-container mt-4">
-          {winner && (
-            <span className="block m-auto mt-4 bg-white text-gray-800 py-2 px-4">
-              🎉 {winner === Players.PLAYER_ONE ? "Player One" : "Player Two"}{" "}
-              won! 🎉
-            </span>
-          )}
-          {inProgress && !playerOneSpherePlaced && (
-            <span className="block m-auto mt-4 bg-white text-gray-800 py-2 px-4">
-              Place Player One's sphere.
-            </span>
-          )}
-          {inProgress && playerOneSpherePlaced && !playerTwoSpherePlaced && (
-            <span className="block m-auto mt-4 bg-white text-gray-800 py-2 px-4">
-              Place Player Two's sphere.
-            </span>
-          )}
-          {inProgressWithSpheresPlaced && (
-            <span className="block m-auto mt-4 bg-white text-gray-800 py-2 px-4">
-              {turn === Players.PLAYER_ONE ? "White" : "Black"}'s turn.
-            </span>
-          )}
-          {!inProgress && (
-            <button
-              className="block m-auto mt-4 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
-              onClick={() => resetGame()}
-            >
-              {winner ? "Play again?" : "Start!"}
-            </button>
-          )}
-        </div>
+        <LocalBoardCaption
+          inProgress={inProgress}
+          setInProgress={setInProgress}
+          winner={winner}
+          setWinner={setWinner}
+          setGameState={setGameState}
+          turn={turn}
+          playerOneSpherePlaced={playerOneSpherePlaced}
+          setPlayerOneSpherePlaced={setPlayerOneSpherePlaced}
+          playerTwoSpherePlaced={playerTwoSpherePlaced}
+          setPlayerTwoSpherePlaced={setPlayerTwoSpherePlaced}
+        />
       </div>
     </div>
   );
