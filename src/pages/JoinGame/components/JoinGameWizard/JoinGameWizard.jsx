a99e9df433d896d3f8619f6c9f68c6b7c50db87f
Sam Warner <sam_warner@talktalk.net>
2022-05-29 13:53:02 +0100

    Extract JoinGameWizard component

---
+++ src/pages/JoinGame/components/JoinGameWizard/JoinGameWizard.jsx
@@ -0,0 +1,149 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const JoinGameWizard = ({
+  baseUrl,
+  username,
+  setUsername,
+  roomCode,
+  setRoomCode,
+  socket,
+  setPlayers,
+  setIsGameStarted,
+}) => {
+  const [isReady, setIsReady] = useState(false);
+  const [isInRoom, setIsInRoom] = useState(false);
+
+  const [errorMessage, setErrorMessage] = useState("");
+  const [hasError, setHasError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const joinParty = () => {
+    socket.emit("setName", username);
+
+    socket.on("joinSuccess", function () {
+      setIsLoading(false);
+      setIsInRoom(true);
+    });
+
+    socket.on("joinFailed", function (err) {
+      setErrorMessage(err);
+      setHasError(true);
+      setIsLoading(false);
+
+      socket.disconnect();
+    });
+
+    socket.on("partyUpdate", (players) => {
+      setPlayers(players);
+    });
+
+    socket.on("startGame", () => {
+      setIsGameStarted(true);
+    });
+
+    socket.on("disconnect", function () {
+      console.log("You've lost connection with the server");
+      socket.close();
+    });
+  };
+
+  const attemptJoinParty = () => {
+    if (!username) {
+      setErrorMessage("Name must be set");
+      setHasError(true);
+      return;
+    }
+
+    if (!roomCode) {
+      setErrorMessage("Room code must be specified");
+      setHasError(true);
+      return;
+    }
+
+    setIsLoading(true);
+
+    axios
+      .get(`${baseUrl}/exists/${roomCode}`)
+      .then(function (res) {
+        if (res.data.exists) {
+          setErrorMessage("");
+          joinParty();
+        } else {
+          setIsLoading(false);
+          setErrorMessage("Invalid Party Code");
+          setHasError(true);
+        }
+      })
+      .catch(function (err) {
+        setIsLoading(false);
+        setErrorMessage("Server error: " + err);
+        setHasError(true);
+      });
+  };
+
+  const reportReady = () => {
+    socket.emit("setReady", true);
+    socket.on("readyConfirm", () => {
+      setIsReady(true);
+    });
+  };
+
+  return (
+    <div className="mb-4">
+      <label
+        className="block text-gray-700 text-sm font-bold mb-2"
+        htmlFor="username"
+      >
+        Username
+      </label>
+      <input
+        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        id="username"
+        type="text"
+        placeholder="Username"
+        disabled={isInRoom}
+        onChange={(evt) => setUsername(evt.target.value)}
+      />
+      <label
+        className="block text-gray-700 text-sm font-bold mb-2"
+        htmlFor="username"
+      >
+        Room Code
+      </label>
+      <input
+        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        id="roomCode"
+        type="text"
+        placeholder="Room Code"
+        disabled={isInRoom}
+        onChange={(evt) => setRoomCode(evt.target.value)}
+      />
+      {hasError && <div className="mt-4">{errorMessage}</div>}
+      {!isLoading && !isInRoom && (
+        <button
+          className="block m-auto mt-4 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+          onClick={() => attemptJoinParty()}
+          disabled={isLoading}
+        >
+          Join game
+        </button>
+      )}
+
+      {isInRoom && !isReady && (
+        <button
+          className="block m-auto mt-4 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+          onClick={() => reportReady()}
+          disabled={isReady}
+        >
+          Ready up!
+        </button>
+      )}
+      {isInRoom && isReady && (
+        <p className="block text-gray-700 m-4">Waiting for host...</p>
+      )}
+    </div>
+  );
+};
+
+export default JoinGameWizard;
