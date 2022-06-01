842b143dedd869bba04026b209c68dfcfad31f04
Sam Warner <sam_warner@talktalk.net>
2022-06-01 11:22:58 +0100

    Add sidepanel to game creator's board

--- src/general/components/Modal/Modal.jsx
+++ src/general/components/Modal/Modal.jsx
@@ -5,7 +5,7 @@ const Modal = ({ type, children }) => {
   const colour = type === "Error" ? "bg-red-200" : "";
 
   return (
-    <div className="fixed w-full h-screen bg-gray-900 bg-opacity-50">
+    <div className="fixed top-0 w-full h-screen bg-gray-900 bg-opacity-50 z-10">
       <div className="modal-dialog relative w-auto m-4 pointer-events-none">
         <div className="modal-content border-none shadow-lg relative flex flex-col w-full pointer-events-auto bg-white bg-clip-padding rounded-md outline-none text-current">
           <div
@@ -18,7 +18,7 @@ const Modal = ({ type, children }) => {
               {type}
             </h5>
           </div>
-          <div className="modal-body relative p-4">{children}</div>
+          <div className="modal-body relative p-4 text-center">{children}</div>
           <div className="modal-footer flex flex-shrink-0 flex-wrap items-center justify-around p-4 border-t border-gray-200 rounded-b-md">
             <Link
               className="block bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
