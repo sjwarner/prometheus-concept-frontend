938c9487dac0077b06a804f89bc38891d5d21621
Sam Warner <sam_warner@talktalk.net>
2022-06-01 18:24:42 +0100

    Allow styling to be passed through to Button component

--- src/general/components/Button/Button.jsx
+++ src/general/components/Button/Button.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-const Button = ({ onClick, disabled, children }) => {
+const Button = ({ className, onClick, disabled, children }) => {
   return (
     <button
-      className="block m-auto mt-4 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow disabled:cursor-not-allowed disabled:opacity-50"
+      className={`block m-auto mt-4 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow disabled:cursor-not-allowed disabled:opacity-50 ${className}`}
       onClick={onClick}
       disabled={disabled}
     >
